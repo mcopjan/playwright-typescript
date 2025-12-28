abad46661ebb04d910c62974bc85ad3302d3abd7
mcopjan <martincopjan@gmail.com>
2025-12-28 14:47:25 +0000

    initial structure for igel12 signing test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('Navigate to a Tenant and login', async ({ page }) => {
-  await page.goto('https://qacloud-three.sip.controlup.com/');
-  await page.getByTestId('EdgeDX_SignInPage_SignInPage_div_40').getByTestId('EdgeDX_SignInPage_SignInPage_input').click();
-  await page.getByTestId('EdgeDX_SignInPage_SignInPage_div_40').getByTestId('EdgeDX_SignInPage_SignInPage_input').fill('testuser@gmail.com');
-  await page.getByTestId('EdgeDX_SignInPage_SignInPage_div_40').getByTestId('EdgeDX_SignInPage_SignInPage_input').press('Tab');
-  await page.getByRole('textbox', { name: 'Enter your password' }).fill('123987123');
-  await page.getByRole('button', { name: 'Sign In' }).click();
-  await expect(page.getByTestId('EdgeDX_ConfirmationModal_ConfirmationModal_p')).toContainText('The username and / or password was invalid!');
-  await page.getByTestId('EdgeDX_ConfirmationModal_ConfirmationModal_button').click();
+//$env:USERNAME=""; $env:PASSWORD=""; npx playwright test --headed --project=chromium --ui
+
+test('Sign_Igel12_IPKG', async ({ page }) => {
+  const username = process.env.USERNAME!;
+  const password = process.env.PASSWORD!;
+  await page.goto('https://appcreator.igel.com/');
+  await page.getByRole('button', { name: 'Let’s login' }).click();
+  await page.getByRole('textbox', { name: 'name@host.com' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.getByRole('button', { name: 'submit' }).click();
+  await expect(page.locator('#root')).toContainText('Create my App');
 });
